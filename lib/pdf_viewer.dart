62074e16a1ed9a9e1c02e1c873c93a53f5c2c41c
Damodar Lohani <dlohani48@gmail.com>
2020-03-18 14:54:18 +0545

    export show IndicatorPosition enum

--- lib/pdf_viewer.dart
+++ lib/pdf_viewer.dart
@@ -2,5 +2,5 @@ library pdf_viewer;
 
 export 'src/document.dart' show PDFDocument;
 export 'src/page.dart' show PDFPage;
-export 'src/viewer.dart' show PDFViewer;
+export 'src/viewer.dart' show PDFViewer, IndicatorPosition;
 export 'src/tooltip.dart' show PDFViewerTooltip;
