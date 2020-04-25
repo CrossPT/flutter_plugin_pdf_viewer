2b1437e0ec25ae1082f7645b87ffb9591750ac3d
Chandra Abdul Fattah <chandraaf28@gmail.com>
2020-04-25 13:17:14 +0700

    Get Packages

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/bezzo/Mobile/flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/bezzo/Mobile/Product/Flutter/flutter_plugin_pdf_viewer/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/bezzo/Mobile/flutter-sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
