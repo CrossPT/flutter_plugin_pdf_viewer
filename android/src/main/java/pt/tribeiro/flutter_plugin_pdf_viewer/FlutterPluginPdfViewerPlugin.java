7d1d8778fdb1fed23d649328de95c3d7975bbc36
Enver ALTIN <enveraltin@users.noreply.github.com>
2021-05-27 12:16:45 +0300

    Workaround for NPE, ignore IDE project files.

--- android/src/main/java/pt/tribeiro/flutter_plugin_pdf_viewer/FlutterPluginPdfViewerPlugin.java
+++ android/src/main/java/pt/tribeiro/flutter_plugin_pdf_viewer/FlutterPluginPdfViewerPlugin.java
@@ -154,8 +154,8 @@ public class FlutterPluginPdfViewerPlugin implements MethodCallHandler {
 
             PdfRenderer.Page page = renderer.openPage(--pageNumber);
 
-            double width = instance.activity().getResources().getDisplayMetrics().densityDpi * page.getWidth();
-            double height = instance.activity().getResources().getDisplayMetrics().densityDpi * page.getHeight();
+            double width = instance.context().getResources().getDisplayMetrics().densityDpi * page.getWidth();
+            double height = instance.context().getResources().getDisplayMetrics().densityDpi * page.getHeight();
             final double docRatio = width / height;
 
             width = 2048;
